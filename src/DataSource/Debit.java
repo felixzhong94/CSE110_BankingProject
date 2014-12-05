32de99a37a002616b0a1e91fa9ca0e3329ad194b
williamlin59 <williamlin59@hotmail.com>
2014-12-04 23:14:24 -0800

    fixed interest rate
    fixed it but the algorithm seems to be not working properly need to
    check

--- src/DataSource/Debit.java
+++ src/DataSource/Debit.java
@@ -260,7 +260,7 @@ public class Debit implements Account {
 
 	@Override
 	public ArrayList<Record> ThirtyDaysRecords(Connection conn) {
-		String query = "select * from Records TimeStamp < NOW() - INTERVAL '10 days' AND AccountNo = ?";
+		String query = "select * from Records where TimeStamp>= ( CURDATE() - INTERVAL 10 DAY ) AND AccountNo = ?";
 		//Record record =new Record();
 		PreparedStatement statement;
 		
